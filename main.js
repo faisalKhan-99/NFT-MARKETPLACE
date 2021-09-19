9b90925f0897ab011fd2df61f12bcc5594eb1b2e
faisalKhan-99 <asdfaisal39@gmail.com>
2021-09-19 15:47:00 +0530

    createnotworking

--- main.js
+++ main.js
@@ -80,9 +80,61 @@ saveUserInfo = async()=>{
 	openUserInfo();
 }
 
+//day 5 
+createItem = async()=>{
+	//did the user select a file?
+	console.log('here');
+	if(createItemFile.files.length==0){
+		alert('Please select a file to upload');
+		return;
+	}
+	//selected a name?
+	else if(createItemNameField.value.length==0){
+		alert('Please specify a name');
+		return;
+	}
+	//create and store on IPFS now
+	const nftFile = new Moralis.File('nftFile.jpg',createItemFile.files[0]);
+	await nftFile.saveIPFS();
+
+	const nftFilePath = nftFile.ipfs();
+	const nftFileHash = nftFile.hash();
+
+	const metadata = {
+		name: createItemNameField.value,
+		description: createItemDescriptionField.value,
+		nftFilePath: nftFilePath,
+		nftFileHash: nftFileHash,
+	}; //now we wanna store this file on IPFS
+
+	const nftFileMetadataFile = new Moralis.File("metadata.json", {base64 : btoa(JSON.stringify(metadata))});
+	await nftFileMetadataFile.saveIPFS();
+
+
+	const nftFileMetadataFilePath = nftFileMetadataFile.ipfs();
+	const nftFileMetadataFileHash = nftFileMetadataFile.hash();
+
+
+	// Simple syntax to create a new subclass of Moralis.Object.
+	const Item = Moralis.Object.extend("Item");
+
+	// Create a new instance of that class.
+	const item = new Item();
+	item.set('name', createItemNameField.value);
+	item.set('description', createItemDescriptionField.value);
+	item.set('nftFilePath', nftFilePath);
+	item.set('nftFileHash', nftFileHash);
+	item.set('MetadataFilePath', nftFileMetadataFilePath);
+	item.set('MetadataFileHash',nftFileMetadataFileHash);
+	await item.save();
+	console.log(item);
+}
+
 
 hideElement = (element) => element.style.display = "none"; //hide somthing on screen
 showElement = (element) => element.style.display = "block";
+
+//Navbar	
 
 const userConnectButton = document.getElementById("btnConnect");
 userConnectButton.onclick = login;
@@ -90,6 +142,12 @@ userConnectButton.onclick = login;
 const userProfileButton= document.getElementById("btnUserInfo");
 userProfileButton.onclick = openUserInfo;
 
+const openCreateItemButton= document.getElementById("btnOpenCreateItem");
+openCreateItemButton.onclick = () =>showElement(createItemForm);
+
+
+
+//user profile
 const userInfo= document.getElementById("userInfo");
 //Day 3
 const userUsernameField= document.getElementById("textUsername");
@@ -102,6 +160,8 @@ document.getElementById("btnCloseUserInfo").onclick = () => hideElement(userInfo
 document.getElementById("btnLogout").onclick = logout;//hook up the funcionality to the button
 document.getElementById("btnSaveUserInfo").onclick = saveUserInfo;
 
+
+//Item Creation
 //Day 4 
 const createItemForm= document.getElementById("createItem");
 
@@ -109,13 +169,12 @@ const createItemNameField= document.getElementById("txtCreateItemName");
 const createItemDescriptionField= document.getElementById("txtCreateItemDescription");
 const createitemPriceField= document.getElementById("numCreateItemPrice");
 const createItemStatusField= document.getElementById("selectCreateItemStatus");
-const createItemFileField= document.getElementById("fileCreateItemFile");
+const createItemFile= document.getElementById("fileCreateItemFile");
 
-
-
-const openCreateItemButton= document.getElementById("btnOpenCreateItem");
-openCreateItemButton.onclick = () =>showElement(createItemForm);
 document.getElementById("btnCloseCreateItem").onclick = () => hideElement(createItemForm);
 
+const createplease = document.getElementById("btnCreateItem");
+createplease.onclick = createItem;
+ 		
 
 init();
